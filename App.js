0f913db91852c7433a18453eec9c11687c3a1915
Sunny Wang <wangjinlian@hotmail.com>
2019-04-07 08:26:02 -0400

    Add SCSS as styling.

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import classes from './App.scss';
+
 class App extends Component{
    render(){
       return(
-         <div>
+         <div className={classes.app}>
             <h1>Hello World</h1>
          </div>
       );
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin =  require('mini-css-extract-plugin');
 
-module.exports = {
-   entry: './main.js',
-   output: {
-      path: path.join(__dirname, '/bundle'),
-      filename: 'index_bundle.js'
-   },
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   },
-   plugins:[
-      new HtmlWebpackPlugin({
-         template: './index.html'
-      })
-   ]
+const miniCssPlugin = new MiniCssExtractPlugin({
+    filename: '[name].[hash].css',
+    chunkFilename:  '[id].[hash].css',
+});
+
+module.exports  = (env,argv) => {
+	return {
+	   entry: './main.js',
+	   output: {
+		  path: path.join(__dirname, '/bundle'),
+		  filename: 'index_bundle.js'
+	   },
+	   devServer: {
+		  inline: true,
+		  port: 8080
+	   },
+	   module: {
+		  rules: [
+			 {
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+				   presets: ['es2015', 'react']
+				}
+			 },{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+					  argv.mode =='development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+					  {
+						loader: 'css-loader',
+						options: {
+						modules: true,
+						importLoaders: 1,
+						localIdentName: "[name]_[local]_[hash:base64:5]",
+						}
+					  },
+					  'sass-loader'
+					]
+				}  
+		  ]
+	   },
+	   plugins:[
+		  new HtmlWebpackPlugin({
+			 template: './index.html'
+		  }), miniCssPlugin
+	   ]
+	}
 }
