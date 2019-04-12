3f94b500dff62048cec5f6e2b2ea5cd8a50f1db7
Sunny Wang <wangjinlian@hotmail.com>
2019-04-11 20:23:35 -0400

    Component API - setState()

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import classes from './App.scss';
 import React from 'react';
 
 class App extends React.Component {
-   constructor(props) {
-      super(props);
+   constructor() {
+      super();
 		
       this.state = {
-         header: "Header from state...",
-         content: "Content from state..."
+         data: []
       }
-   }
+	
+      this.setStateHandler = this.setStateHandler.bind(this);
+   };
+   setStateHandler() {
+      var item = "setState..."
+      var myArray = this.state.data.slice();
+	  myArray.push(item);
+      this.setState({data: myArray})
+   };
    render() {
       return (
          <div>
-            <h1 className={classes.app}>{this.state.header}</h1>
-            <h2>{this.state.content}</h2>
+            <button onClick = {this.setStateHandler}>SET STATE</button>
+            <h4>State Array: {this.state.data}</h4>
          </div>
       );
    }
