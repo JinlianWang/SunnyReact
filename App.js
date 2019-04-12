ebc669a39fa842cd1ada6c50fee4539253dfe8df
Sunny Wang <wangjinlian@hotmail.com>
2019-04-11 20:21:24 -0400

    Example demoing state as part of rendering.

--- App.js
+++ App.js
@@ -2,30 +2,19 @@ import classes from './App.scss';
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <Header/>
-            <Content/>
-         </div>
-      );
+   constructor(props) {
+      super(props);
+		
+      this.state = {
+         header: "Header from state...",
+         content: "Content from state..."
+      }
    }
-}
-class Header extends React.Component {
    render() {
       return (
          <div>
-            <h1 className={classes.app}>Header</h1>
-         </div>
-      );
-   }
-}
-class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
+            <h1 className={classes.app}>{this.state.header}</h1>
+            <h2>{this.state.content}</h2>
          </div>
       );
    }
